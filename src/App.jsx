7658ada2b494a6161bf86aba2044f11e98194aa3
dgalytskyi <galitskiy.dima8@gmail.com>
2025-06-09 18:23:59 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+// import {City} from "./City.jsx";
+// import {Human} from "./Human.jsx";
+// import {Book} from "./Book.jsx";
+// import {MusicBand} from "./MusicBand.jsx";
+import {Recipe} from "./Recipe.jsx";
+
+function App() {
+  return (
+    <>
+      {/*<City></City>*/}
+      {/*<Human></Human>*/}
+      {/*<Book></Book>*/}
+      {/*<MusicBand></MusicBand>*/}
+      <Recipe></Recipe>
+    </>
+  )
+}
+
+export default App
